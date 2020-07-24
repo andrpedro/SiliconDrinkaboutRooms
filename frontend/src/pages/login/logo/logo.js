e229ed995948e12a09a386d11cd1147ac6f7a648
Andre Pedro <andre.pedro.fcamara@duratex.com.br>
2020-07-23 23:11:33 -0300

    Mudança de logo

--- frontend/src/pages/login/logo/logo.js
+++ frontend/src/pages/login/logo/logo.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Logo = () => (
   <div className="row justify-content-center align-items-center">
-    <img src="/images/logo.svg" />
+    <img src="/images/drinkabout-logo-alt@4x.png" />
   </div>
 );
